084bef8ce0d43b4281e50a32b68cd040473262d0
dddog <azskw1101@gmail.com>
2021-11-30 17:22:27 +0900

    init

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { Animated, Pressable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const AnimateContainer = Animated.createAnimatedComponent(Container);
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const [up, setUp] = useState(false);
+  const Y_POSITION = useRef(new Animated.Value(250)).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 250 : -250,
+      useNativeDriver: true,
+      duration: 1000,
+    }).start(toggleUp);
+  };
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-250, -100, 100, 250],
+    outputRange: [1, 0.1, 0.1, 1],
+  });
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-250, 250],
+    outputRange: [100, 0],
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AnimateContainer accessibilityViewIsModal>
+      <AnimatedPressable
+        onPress={moveUp}
+        style={{
+          // flex: 1,
+          // alignItems: "center",
+          // justifyContent: "center",
+          width: 200,
+          height: 200,
+          backgroundColor: "tomato",
+          opacity,
+          borderRadius,
+          transform: [{ translateY: Y_POSITION }],
+        }}
+      >
+        {/* <AnimatedBox
+          style={{
+            opacity,
+            borderRadius,
+            transform: [{ translateY: Y_POSITION }],
+          }}
+        /> */}
+      </AnimatedPressable>
+    </AnimateContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
